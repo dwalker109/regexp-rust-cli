de95467f79f92d890be1e8f4532849ec0500a5fa
Dan Walker <dwalker109@gmail.com>
2021-01-26 00:33:16 +0000

    Port supplied Golang CLI tool to Rust

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,83 @@
-use std::env;
-
 use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::io::Read;
+
+#[derive(Deserialize, Default)]
+#[serde(rename_all = "camelCase", default)]
+struct Input {
+    reg_exp: String,
+    test_string: String,
+    options: Vec<String>,
+    mode: String,
+    substitution: String,
+}
+
+#[derive(Serialize, Default)]
+#[serde(rename_all = "camelCase")]
+struct Output {
+    group_names: Vec<String>,
+    indexes: Vec<Vec<usize>>,
+    substitute_text: String,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let regexp_in = &args[1];
-    let text_in = &args[2];
+    // read from stdin
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
 
-    let regexp = Regex::new(regexp_in);
-    let compiled = match regexp {
-        Ok(r) => r,
-        Err(e) => {
-            panic!(e.to_string())
-        }
+    // define input and output, with defaults
+    let input: Input = serde_json::from_str(&buf).unwrap();
+    let mut output = Output::default();
+
+    // setup options
+    let is_global = input.options.contains(&String::from("g"));
+    let options: Vec<String> = input
+        .options
+        .into_iter()
+        .filter(|o| o != &String::from("g"))
+        .collect();
+    let options_fmt = match options.len() {
+        0 => String::from(""),
+        _ => format!("(?{})", options.join("")),
     };
 
-    let result = compiled.is_match(text_in);
+    // format and compile the regexp
+    let regexp = format!("{}{}", options_fmt, input.reg_exp);
+    let compiled = Regex::new(&regexp).unwrap();
 
-    println!("Result: {}", result);
+    // get the group names
+    output.group_names = compiled
+        .capture_names()
+        .map(|c| match c {
+            Some(name) => String::from(name),
+            None => String::from(""),
+        })
+        .collect();
+
+    // get matches
+    if is_global {
+        output.indexes = compiled
+            .find_iter(&input.test_string)
+            .map(|m| vec![m.start(), m.end()])
+            .collect();
+    } else {
+        output.indexes = match compiled.find(&input.test_string) {
+            Some(m) => vec![vec![m.start(), m.end()]],
+            None => Vec::new(),
+        }
+    }
+
+    // get substitutions
+    if input.mode == "Substitution" {
+        let n = match is_global {
+            true => 0,
+            false => 1,
+        };
+        output.substitute_text = compiled
+            .replacen(&input.test_string, n, &input.substitution[..])
+            .to_string();
+    }
+
+    // write to stdout
+    print!("{}", serde_json::to_string(&output).unwrap());
 }
